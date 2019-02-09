a95dd77fd3168a716e6521213be3e309d43c8c8a
Alex Belke <alex.belke.de@gmail.com>
2019-02-09 21:28:24 +0100

    check props

--- src/components/Congrats/Congrats.js
+++ src/components/Congrats/Congrats.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Functional react component for congratulatory message.
  * @function
  * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
  */
-export default (props) => {
+const Congrats = (props) => {
     if (props.success) {
         return (<div data-test="component-congrats">
             <span data-test="congrats-message">Congratulations! You guessed the word!</span>
@@ -16,3 +17,9 @@ export default (props) => {
         )
     }
 }
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired
+}
+
+export default Congrats;
